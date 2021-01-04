6083b18a1159c44e1c063d60c57b750379a082bd
TY-Vishnu-MowdgalayaBM <76555499+TY-Vishnu-MowdgalayaBM@users.noreply.github.com>
2021-01-04 13:33:05 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function dis(val) {
+    document.getElementById("edu").value += val
+}
+//function for evaluation
+function solve() {
+    let x = document.getElementById("edu").value
+    let y = eval(x)
+    document.getElementById("edu").value = y
+}
+//function for clearing the display
+function clr() {
+    document.getElementById("edu").value = ""
+}
